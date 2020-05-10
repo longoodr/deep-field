742d160c7e52cc0fc47ee09d4e4cd58381ccc729
Ryan Longood <longoodr@gmail.com>
2020-05-10 13:12:40 -0400

    working game page team and venue queries

--- deepfield/data/pages.py
+++ deepfield/data/pages.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Iterable
+from typing import Iterable, List, Tuple
 
 import requests
 from bs4 import BeautifulSoup
+from peewee import Query
 
-from deepfield.data.dbmodels import DeepFieldModel
+from deepfield.data.dbmodels import Team, Venue
 from deepfield.data.dependencies import DependencyResolver
 
 
@@ -27,13 +28,12 @@ class Page(ABC):
         for url in self.get_referenced_page_urls():
             if not self._dep_res.is_url_resolved(url):
                 raise ValueError(f"Dependency for {url} not resolved")
-        for model in self._get_models_to_add():
-            model.save()
+        self._run_queries()
         
     @abstractmethod
-    def _get_models_to_add(self) -> Iterable[DeepFieldModel]:
-        """Enumerates all models that exist on the page that need to be 
-        inserted into the database.
+    def _run_queries(self) -> None:
+        """Enumerates all queries that need to be executed to properly insert
+        this page into the database.
         """
         pass
     
@@ -52,8 +52,8 @@ class BBRefPage(Page):
 class SchedulePage(BBRefPage):
     """A page containing a set of URLs corresponding to game pages."""
     
-    def _get_models_to_add(self) -> Iterable[DeepFieldModel]:
-        return []
+    def _run_queries(self) -> None:
+        return
     
     def get_referenced_page_urls(self) -> Iterable[str]:
         urls = []
@@ -62,3 +62,51 @@ class SchedulePage(BBRefPage):
             suffix = game.em.a["href"]
             urls.append(self.base_url + suffix)
         return urls
+
+class GamePage(BBRefPage):
+    """A page corresponding to the play-by-play info for a game."""
+    
+    def __init__(self, html: str, dep_res: DependencyResolver):
+        super().__init__(html, dep_res)
+        self._scorebox = self._soup.find("div", {"class": "scorebox"})
+        self._scorebox_meta = self._scorebox.find("div", {"class": "scorebox_meta"})
+    
+    def _run_queries(self) -> None:
+        # TODO: should add the teams, venue, game, contained plays in that order
+        teams = self._add_teams()
+        venue = self._add_venue()
+    
+    def _add_venue(self) -> Venue:
+        name = self._get_venue_name()
+        venue, _ = Venue.get_or_create(name=name)
+        return venue
+        
+    def _get_venue_name(self) -> str:
+        venue_div_filter = lambda div: div.text.startswith("Venue: ")
+        venue_div = self._scorebox_meta.find(venue_div_filter)
+        return venue_div.text.split(": ")[1] # "Venue: <venue name>"
+    
+    def _add_teams(self) -> Iterable[Team]:
+        teams = []
+        info = self._get_team_info()
+        for name, abbreviation in info:
+            team, _ = Team.get_or_create(name=name, abbreviation=abbreviation)
+            teams.append(team)
+        return teams
+    
+    def _get_team_info(self) -> Iterable[Tuple[str, str]]:
+        """Returns a 2 element list, which contains tuples of the name and
+        abbreviation for away, home teams respectively
+        """
+        team_divs = self._scorebox.find_all("div", recursive=False, limit=2)
+        info: List[Tuple[str, str]] = []
+        for td in team_divs:
+            team_info = td.div.strong.a
+            suffix = team_info["href"] # /teams/abbreviation/year.html
+            abbreviation = suffix.split("/")[2]
+            name = str(team_info.string)
+            info.append((name, abbreviation))
+        return info
+    
+    def get_referenced_page_urls(self) -> Iterable[str]:
+        return [] # TODO: should be player URLs
--- tests/data/test_pages.py
+++ tests/data/test_pages.py
@@ -1,24 +1,46 @@
 from pathlib import Path
 
 import pytest
+from peewee import SqliteDatabase
 
+from deepfield.data.dbmodels import Game, Play, Player, Team, Venue
 from deepfield.data.dependencies import IgnoreDependencies
-from deepfield.data.pages import SchedulePage
+from deepfield.data.pages import GamePage, SchedulePage
 
+test_db = SqliteDatabase(":memory:")
+MODELS = [Game, Play, Player, Team, Venue]
 
-class TestSchedulePage():
+def get_res_path(name: str) -> Path:
+    base_path = Path(__file__).parent
+    return (base_path / ("resources/" + name)).resolve()
+
+def setup_module(module):
+    test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+    test_db.connect()
+    test_db.create_tables(MODELS)
+
+def teardown_module(module):
+    test_db.drop_tables(MODELS)
+    test_db.close()
+
+class TestPage:
     
     @classmethod
     def setup_method(cls):
         base_path = Path(__file__).parent
-        file_path = (base_path / "resources/2016schedule.html").resolve()
-        with open(file_path, "r", encoding="utf-8") as sched_file:
-            html = sched_file.read()
-            sched = SchedulePage(html, IgnoreDependencies())
-            cls.page_urls = sched.get_referenced_page_urls()
-            cls.models = sched._get_models_to_add()
-            
-    def test_urls_on_page(self):
+        file_path = (get_res_path(cls.name)).resolve()
+        with open(file_path, "r", encoding="utf-8") as page_file:
+            html = page_file.read()
+            page = cls.page_type(html, IgnoreDependencies())
+            cls.page_urls = page.get_referenced_page_urls()
+            page._run_queries()
+
+class TestSchedulePage(TestPage):
+    
+    name = "2016schedule.html"
+    page_type = SchedulePage
+    
+    def test_urls(self):
         base_url = "https://www.baseball-reference.com"
         on_list_games = [
             "/boxes/KCA/KCA201604030.shtml",
@@ -36,6 +58,16 @@ class TestSchedulePage():
         not_on_list = [base_url + suffix for suffix in not_on_list]
         for url in not_on_list:
             assert url not in self.page_urls
-            
-    def test_no_models(self):
-        assert len(self.models) == 0
+
+class TestGamePage(TestPage):
+    
+    name = "WAS201710120.shtml"
+    page_type = GamePage
+
+    def test_urls(self):
+        pass
+
+    def test_queries(self):
+        Venue.get(Venue.name == "Nationals Park")
+        Team.get(Team.name == "Washington Nationals" and Team.abbreviation == "WSN")
+        Team.get(Team.name == "Chicago Cubs" and Team.abbreviation == "CHC")
