835085f6c443d562c6b50fda9b9016cf7636aa1b
Ryan Longood <longoodr@gmail.com>
2020-05-12 17:17:38 -0400

    implement untested pbp insertion

--- deepfield/data/pages.py
+++ deepfield/data/pages.py
@@ -1,12 +1,13 @@
+import re
 from abc import ABC, abstractmethod
 from datetime import date, datetime, time
-from typing import Callable, Dict, Iterable, List, Set, Tuple
+from typing import Any, Callable, Dict, Iterable, List, Set, Tuple
 
 import requests
 from bs4 import BeautifulSoup, Comment
 from peewee import Query
 
-from deepfield.data.dbmodels import Game, Team, Venue
+from deepfield.data.dbmodels import Game, Play, Team, Venue
 from deepfield.data.dependencies import DependencyResolver
 from deepfield.data.enums import FieldType, OnBase, TimeOfDay
 
@@ -82,7 +83,7 @@ class GamePage(BBRefPage):
     def get_referenced_page_urls(self) -> Iterable[str]:
         player_suffixes = []
         for table in self._player_tables:
-            player_suffixes += table.get_suffixes()
+            player_suffixes += table.get_page_suffixes()
         return [self.base_url + s for s in player_suffixes]
     
     @staticmethod
@@ -95,6 +96,21 @@ class GamePage(BBRefPage):
         """
         table_contents = ph_div.next_sibling.next_sibling
         return BeautifulSoup(table_contents, "lxml")
+    
+    """There are a few edge cases for name lookups, since canonical player
+    names and the names presented in play rows vary slightly. Players known
+    with middle initials and/or with Jr./Sr. title may or may not be
+    represented in play rows with these name elements. This allows names to be
+    standardized across these different presentations.
+    """
+    _NAME_TITLE = re.compile(r" [J|S]r\.")
+    _MIDDLE_INITIAL = re.compile(r" \w\.")
+
+    @classmethod
+    def _get_stripped_name(cls, name: str) -> str:
+        name_wo_mid = re.sub(cls._MIDDLE_INITIAL, "", name)
+        fully_stripped = re.sub(cls._NAME_TITLE, "", name_wo_mid)
+        return fully_stripped
         
     class PlayerTables:
         """Manages access to the tables of away and home players for the given
@@ -121,7 +137,7 @@ class GamePage(BBRefPage):
     class PlayerTable:
         """Manages access to a table of players."""
         
-        _player_tag_attr_filter = {
+        _PLAYER_TAG_ATTR_FILTER = {
             "data-stat": "player",
             "scope": "row"
         }
@@ -129,16 +145,30 @@ class GamePage(BBRefPage):
         def __init__(self, placeholder):
             self._table = GamePage._get_table_from_placeholder(placeholder)
                 
-        def get_suffixes(self) -> List[str]:
-            return [row.a["href"] for row in self.get_rows()]
+        def get_page_suffixes(self) -> List[str]:
+            return [self._get_page_suffix(row) for row in self.get_rows()]
         
         def get_name_map(self) -> Dict[str, str]:
-            return {row.a.text.replace(u"\xa0", u" "): row.a["href"] for row in self.get_rows()}
+            return {self._get_player_name(row): self._get_player_id(row) for row in self.get_rows()}
+        
+        @staticmethod
+        def _get_player_name(row) -> str:
+            canonical_name = row.a.text.replace(u"\xa0", u" ")
+            return GamePage._get_stripped_name(canonical_name)
+        
+        @staticmethod
+        def _get_player_id(row) -> str:
+            page_suffix = GamePage.PlayerTable._get_page_suffix(row)
+            return page_suffix.split("/")[-1].split(".")[0] # smithjo01
+        
+        @staticmethod
+        def _get_page_suffix(row) -> str:
+            return row.a["href"] # /players/s/smithjo01.shtml
         
         def get_rows(self):
             return self._table.find_all(
                 self._player_tag_filter,
-                attrs=self._player_tag_attr_filter
+                attrs=self._PLAYER_TAG_ATTR_FILTER
                 )
                     
         @staticmethod 
@@ -217,16 +247,17 @@ class GamePage(BBRefPage):
     
         def add_game(self, teams: List[Team], venue: Venue) -> Game:
             fields = {
-                "name_id": self._name,
+                "name_id"         : self._name,
                 "local_start_time": self._get_local_start_time(),
-                "time_of_day": self._enum_to_int(self._get_time_of_day()),
-                "field_type": self._enum_to_int(self._get_field_type()),
-                "date": self._get_date(),
-                "venue_id": venue.id,
-                "away_team_id": teams[0].id,
-                "home_team_id": teams[1].id,
+                "time_of_day"     : self._enum_to_int(self._get_time_of_day()),
+                "field_type"      : self._enum_to_int(self._get_field_type()),
+                "date"            : self._get_date(),
+                "venue_id"        : venue.id,
+                "away_team_id"    : teams[0].id,
+                "home_team_id"    : teams[1].id,
             }
             game, _ = Game.get_or_create(**fields)
+            return game
             
         @staticmethod
         def _enum_to_int(enum):
@@ -289,84 +320,22 @@ class GamePage(BBRefPage):
 
     class PlayQueryRunner:
         
-        PBP_TO_DB_STATS: Dict[str, Tuple[str, Callable]]
-        PLAYERS: Set[str]
-        PBP_STATS: Set[str]
-        INNING_CHAR_OFFSET: Dict[str, int]
-        
         def __init__(self, soup, player_tables):
             self._soup = soup
             self._pbp_table = self._get_pbp_table()
-            self._home_map = player_tables.home.get_name_map()
-            self._away_map = player_tables.away.get_name_map()
-            self._init_class_vars()
-            
-        # FIXME this approach won't work, as player lookups require knowledge
-        # of whether they're on the home or away team
-        def _init_lookups(self):
-            if hasattr(self, "PBP_TO_DB_STATS"):
-                return
-            self.PBP_TO_DB_STATS = {
-                "inning":               ("inning_half"  , self._inning_to_inning_half),
-                "pitches_pbp":          ("pitch_ct"     , lambda x: x),
-                "play_desc":            ("desc"         , lambda x: x),
-                "runners_on_bases_pbp": ("start_on_base", self._runners_to_on_base),
-                "outs":                 ("start_outs"   , lambda x: int(x)),
-                "batter":               ("batter_id"    , self._batter_to_id),
-                "pitcher":              ("pitcher_id"   , self._pitcher_to_id),
-            }
-            self.PLAYERS = set([
-                "batter",
-                "pitcher",
-                ])
-            self.PBP_STATS = set(self.PBP_TO_DB_STATS.keys())
-            self.INNING_CHAR_OFFSET = {
-                "t": 0,
-                "b": 1
-            }
+            self._transformer = GamePage.PlayQueryRunner.PlayDataTransformer(player_tables)
+
                 
         def add_plays(self, game: Game) -> None:
-            for play_row in self._get_play_rows():
-                self._add_play(play_row)
+            for play_num, play_row in enumerate(self._get_play_rows()):
+                self._add_play(play_row, play_num, game)
             
-        def _add_play(self, play_row) -> None:
-            raw_play_data = self._extract_raw_play_data(play_row)
-            new_data = self._transform_raw_play_data(raw_play_data)
-            pass
-            
-        def _extract_raw_play_data(self, play_row) -> Dict[str, str]:
-            raw_play_data: Dict[str, str] = {}
-            for play_data_pt in play_row.find_all():
-                data_stat = str(play_data_pt.get("data-stat"))
-                if data_stat in self.PBP_STATS:
-                    raw_play_data[data_stat] = play_data_pt.text.replace(u"\xa0", u" ")
-            return raw_play_data
-        
-        def _transform_raw_play_data(self, raw_play_data) -> Dict[str, str]:
-            new_data: Dict[str, str] = {}
-            for pbp_statname, (db_statname, transform_func) in self.PBP_TO_DB_STATS.items():
-                new_data[db_statname] = transform_func(raw_play_data[pbp_statname])
-            return new_data
-        
-        @classmethod
-        def _inning_to_inning_half(cls, inning: str) -> int:
-            inning_num = int(inning[1:])
-            inning_half_char = inning[0]
-            return 2 * (inning_num - 1) + cls.INNING_CHAR_OFFSET[inning_half_char]
-        
-        @staticmethod
-        def _runners_to_on_base(runners: str) -> int:
-            on_base = 0
-            for base, on_base_flag in zip(runners, [OnBase.FIRST, OnBase.SECOND, OnBase.THIRD]):
-                if not base == "-":
-                    on_base += on_base_flag.value
-            return on_base
-        
-        def _batter_to_id(self, batter_name: str) -> str:
-            pass
-        
-        def _pitcher_to_id(self, pitcher_name: str) -> str:
-            pass
+        def _add_play(self, play_row, play_num: int, game: Game) -> None:
+            raw_play_data = self._transformer.extract_raw_play_data(play_row)
+            play_data = self._transformer.transform_raw_play_data(raw_play_data)
+            play_data["game_id"] = game.id
+            play_data["play_num"] = play_num
+            Play.get_or_create(**play_data)
             
         def _get_pbp_table(self):
             # pbp table placeholder is the 7th on page
@@ -379,3 +348,121 @@ class GamePage(BBRefPage):
                 "tr",
                 id=lambda id: id and id.startswith("event_")
                 )
+            
+        class PlayDataTransformer:
+            
+            PBP_TO_DB_STATS: Dict[str, Tuple[str, Callable]]
+            PLAYERS: Dict[str, Tuple[str, Callable]]
+            PBP_STATS: Set[str]
+            INNING_CHAR_OFFSET: Dict[str, int]
+            INNING_AND_PLAYER_TO_SIDE: Dict[Tuple[str, str], str]
+            
+            def __init__(self, player_tables):
+                self._init_lookups()
+                self._player_maps = {
+                    "home": player_tables.home.get_name_map(),
+                    "away": player_tables.away.get_name_map(),
+                }
+        
+            @classmethod
+            def _init_lookups(cls):
+                """Initializes dicts to lookup database stat names and
+                transformation functions to translate page data to database format.
+                This is a class method so it is only done once, as this would be a
+                bit expensive to do for every page.
+                """
+                if hasattr(cls, "PBP_TO_DB_STATS"):
+                    return
+                # these functions can translate to db data with only raw data as input
+                cls.PBP_TO_DB_STATS = {
+                    "inning":               ("inning_half"  , cls._inning_to_inning_half),
+                    "pitches_pbp":          ("pitch_ct"     , cls._no_transformation_needed),
+                    "play_desc":            ("desc"         , cls._no_transformation_needed),
+                    "runners_on_bases_pbp": ("start_on_base", cls._runners_to_on_base),
+                    "outs":                 ("start_outs"   , cls._convert_to_int),
+                }
+                # these functions require knowledge of inning half to determine if home or away player
+                cls.PLAYERS = {
+                    "batter":               ("batter_id"    , cls._batter_to_id),
+                    "pitcher":              ("pitcher_id"   , cls._pitcher_to_id),
+                }
+                cls.INNING_CHAR_OFFSET = {
+                    "t": 0,
+                    "b": 1
+                }
+                # home team gets to bat in second half of inning (b)
+                cls.INNING_AND_PLAYER_TO_SIDE = {
+                    ("t", "batter"): "away",
+                    ("b", "batter"): "home",
+                    ("t", "pitcher"): "home",
+                    ("b", "pitcher"): "away",
+                }
+                cls.PBP_STATS = set(cls.PBP_TO_DB_STATS.keys()).union(set(cls.PLAYERS.keys()))
+                
+                
+            def extract_raw_play_data(self, play_row) -> Dict[str, str]:
+                raw_play_data: Dict[str, str] = {}
+                for play_data_pt in play_row.find_all():
+                    data_stat = str(play_data_pt.get("data-stat"))
+                    if data_stat in self.PBP_STATS:
+                        raw_play_data[data_stat] = play_data_pt.text.replace(u"\xa0", u" ")
+                return raw_play_data
+            
+            def transform_raw_play_data(self, raw_play_data: Dict[str, str]) -> Dict[str, Any]:
+                transformed_stats = self._transform_stats(raw_play_data)
+                self._insert_player_ids(raw_play_data, into_dict=transformed_stats)
+                return transformed_stats
+            
+            def _transform_stats(self, raw_play_data: Dict[str, str]) -> Dict[str, str]:
+                new_data: Dict[str, str] = {}
+                for pbp_statname, (db_statname, transform_func) in self.PBP_TO_DB_STATS.items():
+                    new_data[db_statname] = transform_func(self, raw_play_data[pbp_statname])
+                return new_data
+            
+            def _insert_player_ids(self, raw_play_data: Dict[str, str], into_dict: Dict[str, str]) -> Dict[str, str]:
+                inning_half_char = raw_play_data["inning"][0]
+                for player_type, (player_type_id, player_lookup_func) in self.PLAYERS.items():
+                    player_name = raw_play_data[player_type]
+                    into_dict[player_type_id] = player_lookup_func(self, player_name, inning_half_char)
+                return into_dict
+            
+            def _inning_to_inning_half(self, inning: str) -> int:
+                #[t|b][0-9]+ (t1, b2, t11, etc)
+                inning_num = int(inning[1:])
+                inning_half_char = inning[0]
+                # 0-indexed (t1 -> 0; b1 -> 1; b2 -> 2 etc)
+                return 2 * (inning_num - 1) + self.INNING_CHAR_OFFSET[inning_half_char]
+            
+            def _runners_to_on_base(self, runners: str) -> int:
+                #[-|1][-|2][-|3] where - means nobody on base (---, 1-3, 12-, etc)
+                on_base = 0
+                for base, on_base_flag in zip(runners, [OnBase.FIRST, OnBase.SECOND, OnBase.THIRD]):
+                    if not base == "-":
+                        on_base += on_base_flag.value
+                return on_base
+            
+            def _batter_to_id(self, batter_name: str, inning_half_char: str) -> str:
+                return self._player_to_id(batter_name, inning_half_char, "batter")
+            
+            def _pitcher_to_id(self, pitcher_name: str, inning_half_char: str) -> str:
+                return self._player_to_id(pitcher_name, inning_half_char, "pitcher")
+            
+            def _player_to_id(self, player_name: str, inning_half_char: str, player_type: str) -> str:
+                side = self.INNING_AND_PLAYER_TO_SIDE[(inning_half_char, player_type)]
+                pmap = self._player_maps[side]
+                try:
+                    # first try unedited name, since there's some overhead w/ stripping
+                    return pmap[player_name]
+                except KeyError:
+                    stripped_name = GamePage._get_stripped_name(player_name)
+                    return pmap[stripped_name]
+                
+                # if all attempts fail, there's a new edge case that needs to be
+                # considered
+                raise KeyError(player_name)
+            
+            def _no_transformation_needed(self, stat: str) -> str:
+                return stat
+            
+            def _convert_to_int(self, stat: str) -> int:
+                return int(stat)
