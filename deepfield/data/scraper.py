039dba60cf76a026e422af7e365ff78d4ff8c32b
Ryan Longood <longoodr@gmail.com>
2020-05-13 19:33:34 -0400

    HtmlRetrieval skeleton

--- deepfield/data/scraper.py
+++ deepfield/data/scraper.py
@@ -1,4 +1,5 @@
-from typing import Type
+from abc import ABC, abstractmethod
+from typing import Iterable, Optional, Type
 
 from deepfield.data.bbref_pages import (BBRefLink, BBRefPage, GamePage,
                                         PlayerPage, SchedulePage)
@@ -9,25 +10,49 @@ class BBRefPageFactory:
     """Creates BBRefPages from BBRefLinks."""
     
     def create_page_from_link(self, link: BBRefLink) -> BBRefPage:
-        page_type = self.__get_link_page_type(link)
-        html = HTMLRetriever(link).retrieve_html()
+        page_type = link.get_page_type()
+        html = HtmlRetriever(link).retrieve_html()
+        if html is None:
+            raise ValueError(f"Could not get HTML for {link}")
         return page_type(html)
     
-    @classmethod
-    def __get_link_page_type(cls, link: BBRefLink) -> Type[BBRefPage]:
-        if "boxes" in link._url:
-            return GamePage
-        elif "players" in link._url:
-            return PlayerPage
-        elif "schedule" in link._url:
-            return SchedulePage
-        raise ValueError(f"Couldn't determine page type of {link}")
-    
-class HTMLRetriever:
-    """Retrieves HTML associated with the given link."""
+class AbstractHtmlRetrievalHandler(ABC):
+    """A step in the HTML retrieval process."""
     
     def __init__(self, link: BBRefLink):
         self._link = link
-        
-    def retrieve_html(self) -> str:
+    
+    @abstractmethod
+    def retrieve_html(self) -> Optional[str]:
+        """Returns HTML if successful, or None if not."""
+        pass
+ 
+class CachedHandler(AbstractHtmlRetrievalHandler):
+    """Retrieves HTML associated with the given link from local cache."""
+
+    def retrieve_html(self) -> Optional[str]:
         raise NotImplementedError
+    
+class WebHandler(AbstractHtmlRetrievalHandler):
+    """Retrieves HTML associated with the given link from the web."""
+    
+    def retrieve_html(self) -> Optional[str]:
+        raise NotImplementedError
+    
+class HtmlRetriever(AbstractHtmlRetrievalHandler):
+    """Retrieves HTML associated with the given link."""
+    
+    __HANDLER_SEQUENCE = [
+        CachedHandler,
+        WebHandler
+    ]
+    
+    def __init__(self, link: BBRefLink):
+        super().__init__(link)
+        
+    def retrieve_html(self) -> Optional[str]:
+        for handler_type in self.__HANDLER_SEQUENCE:
+            html = handler_type(self._link).retrieve_html()
+            if html is not None:
+                return html
+        return None
