b1f2235c2cee3ee952c0f967cf2a56b4e4cd0071
Ryan Longood <longoodr@gmail.com>
2020-05-25 17:27:19 -0500

    first draft of PlayerRatings

---
+++ deepfield/model/ratings.py
@@ -0,0 +1,40 @@
+from typing import Dict
+
+import numpy as np
+
+
+class PlayerRatings:
+    """A set of ratings for players that can be updated as plays are 
+    evaluated.
+    """
+
+    def __init__(self, num_stats: int):
+        self._num_stats = 0
+        self.reset()
+
+    def copy(self) -> "PlayerRatings":
+        """Returns a copy of this PlayerRatings."""
+        cp = PlayerRatings(self._num_stats)
+        cp._bratings = self._copy_ratings(self._bratings)
+        cp._pratings = self._copy_ratings(self._pratings)
+        return cp
+    
+    @staticmethod
+    def _copy_ratings(ratings: Dict[int, np.ndarray]) -> Dict[int, np.ndarray]:
+        return {id_: r for id_, r in ratings.items()}
+
+    def reset(self):
+        """Zeroes all player ratings."""
+        self._bratings: Dict[int, np.ndarray] = {}
+        self._pratings: Dict[int, np.ndarray] = {}
+
+    def update(self, kl_div: float, delta: np.ndarray, bid: int, pid: int)\
+            -> None:
+        """Updates the ratings for the given players."""
+        scaled_delta = kl_div * delta
+        if bid not in self._bratings:
+            self._bratings[bid] = np.zeros(self._num_stats)
+        if pid not in self._pratings:
+            self._pratings[pid] = np.zeros(self._num_stats)
+        self._bratings[bid] += scaled_delta
+        self._pratings[pid] -= scaled_delta
