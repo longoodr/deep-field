8388aa52dd7ea1ef94d85e983319f6f5268608b4
Ryan Longood <longoodr@gmail.com>
2020-05-25 14:28:43 -0500

    TestTransitionGenotype skeleton

---
+++ tests/model/test_transition.py
@@ -0,0 +1,15 @@
+import pytest
+
+from deepfield.model.transition import TransitionGenotype
+
+
+class TestTransitionGenotype:
+
+    def test_access(self):
+        pass
+
+    def test_mutation(self):
+        pass
+
+    def test_crossover(self):
+        pass
