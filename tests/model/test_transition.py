c287b8aa84791dac063115612021e67dabb2a3b0
Ryan Longood <longoodr@gmail.com>
2020-05-25 14:34:13 -0500

    test access

--- tests/model/test_transition.py
+++ tests/model/test_transition.py
@@ -1,12 +1,18 @@
 import pytest
 
+from deepfield.enums import Outcome
 from deepfield.model.transition import TransitionGenotype
 
 
 class TestTransitionGenotype:
 
     def test_access(self):
-        pass
+        g = TransitionGenotype.get_random_genotype(3)
+        for o in Outcome:
+            val = o.value
+            assert g._vecs[val] is g[val]
+            assert g._vecs[val] is g[o]
+            
 
     def test_mutation(self):
         pass
