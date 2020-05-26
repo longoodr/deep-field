0510a16355d7a5013f488714e5b1d75648ba727a
Ryan Longood <longoodr@gmail.com>
2020-05-26 15:58:02 -0400

    return namedtuples; log iteration

--- deepfield/playgraph/retrieval.py
+++ deepfield/playgraph/retrieval.py
@@ -94,10 +94,13 @@ class _DbPlaysToGraphIterator():
     """
 
     def __iter__(self):
-        self._plays = iter(self.__get_plays())
+        query = self.__get_plays()
+        self._plays = iter(query)
+        self._ct = query.count()
         # maps player id to level of their last play + 1 (i.e. 0 => no plays)
         # (+ 1, because node levels should be 0-indexed)
         self._player_to_lvl: CounterType[int] = Counter()
+        self._num = 0
         return self
 
     def __next__(self) -> Node:
@@ -105,6 +108,9 @@ class _DbPlaysToGraphIterator():
         while node is None:
             play = next(self._plays)
             node = self._get_node_from_play(play)
+        self._num += 1
+        if self._num % 1000 == 0:
+            logger.info(f"{self._num} of roughly {self._ct} nodes processed")
         return node
 
     def _get_node_from_play(self, play) -> Optional[Node]:
@@ -112,13 +118,13 @@ class _DbPlaysToGraphIterator():
         if outcome is None:
             return None
         level = self._get_node_level(play)
-        self._player_to_lvl[play.batter_id_id] = level + 1
-        self._player_to_lvl[play.pitcher_id_id] = level + 1
+        self._player_to_lvl[play.batter_id] = level + 1
+        self._player_to_lvl[play.pitcher_id] = level + 1
         return (play.id, outcome.value, level)
 
     def _get_node_level(self, play) -> int:
-        bid = play.batter_id_id
-        pid = play.pitcher_id_id
+        bid = play.batter_id
+        pid = play.pitcher_id
         b_lvl = self._player_to_lvl[bid]
         p_lvl = self._player_to_lvl[pid]
         return max(b_lvl, p_lvl)
@@ -129,6 +135,7 @@ class _DbPlaysToGraphIterator():
                 .select(Play.id, Play.batter_id, Play.pitcher_id, Play.desc)
                 .join(Game, on=(Play.game_id == Game.id))
                 .order_by(Game.date, Play.game_id, Play.play_num)
+                .namedtuples()
             )
 
 class _ChecksumGenerator:
