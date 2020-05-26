ddf80e86def87a5cb0821869d64ebe68671adefb
Ryan Longood <longoodr@gmail.com>
2020-05-26 13:22:11 -0400

    change persistor interface

--- deepfield/playgraph/getters.py
+++ deepfield/playgraph/getters.py
@@ -16,24 +16,25 @@ from deepfield.playgraph.graph import Node, EdgeList
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-class PlayGraphPersistenceChecker:
-    """Will try returning an on-disk graph if the database's checksum matches
-    the checksum for the on-disk graph at its time of creation; otherwise, will
-    build the graph from scratch.
+class PlayGraphPersistor:
+    """Maintains a consistent on-disk graph by check consistency and rewriting
+    the graph if inconsistent.
     """
 
     def __init__(self):
         db_name = os.path.splitext(get_db_name())[0]
         self._hash_filename = f"{db_name}_playgraph_hash.txt"
 
-    def get_graph(self) -> nx.DiGraph:
-        od_graph = self._get_on_disk_graph()
-        if od_graph is not None:
-            return od_graph
-        graph = PlayGraphDbIterator().get_graph()
-        PlayGraphDbWriter(graph).save_graph()
-        self._write_hash()
-        return graph
+    def ensure_consistency(self) -> bool:
+        """If inconsistent, rewrites the graph; returns whether the graph was
+        rewritten. After calling this, the graph is guaranteed to be
+        consistent.
+        """
+        if self.is_on_disk_consistent():
+            return False
+        PlayGraphDbWriter().write_graph()
+        self._write_graph_hash()
+        return True
 
     def remove_files(self) -> None:
         clean_graph()
@@ -44,21 +45,17 @@ class PlayGraphPersistenceChecker:
 
     def is_on_disk_consistent(self) -> bool:
         """Returns whether the saved graph is consistent with the database."""
-        return self._matches_db_hash()
+        return self._graph_and_db_hashes_match()
 
-    def _get_on_disk_graph(self) -> Optional[nx.DiGraph]:
-        """If the on-disk graph is consistent with the database, return it;
-        otherwise return None.
-        """
-        if not self.is_on_disk_consistent():
-            return None
-        return _PlayGraphDbReader().get_graph()
-
-    def _matches_db_hash(self) -> bool:
-        hash_ = self._get_graph_hash()
-        if hash_ is None:
+    def _graph_and_db_hashes_match(self) -> bool:
+        graph_hash = self._get_graph_hash()
+        if graph_hash is None:
             return False
-        return self._get_db_hash() == hash_
+        try:
+            db_hash = self._get_db_hash()
+        except FileNotFoundError:
+            return False
+        return db_hash == graph_hash
 
     def _get_graph_hash(self) -> Optional[str]:
         try:
@@ -67,12 +64,12 @@ class PlayGraphPersistenceChecker:
         except FileNotFoundError:
             return None
 
-    def _write_hash(self) -> None:
+    def _write_graph_hash(self) -> None:
         with open(self._hash_filename, "w") as hash_file:
             # file must exist at this point (would have exited if not)
             hash_file.write(self._get_db_hash())    # type: ignore
 
-    def _get_db_hash(self) -> Optional[str]:
+    def _get_db_hash(self) -> str:
         return _ChecksumGenerator(get_db_name()).get_checksum()
 
 class _PlayGraphDbReader:
@@ -104,50 +101,24 @@ class _PlayGraphDbReader:
             interval_logger.log(i)
 
 class PlayGraphDbWriter:
-    """Writes a graph to the database."""
+    """Reads plays from the database and writes the corresponding play graph to
+    the database.
+    """
 
-    __NODES_PER_BATCH = 400
-    __EDGES_PER_BATCH = 400
+    __PER_BATCH = 400
 
-    def save_graph(self) -> None:
+    def write_graph(self) -> None:
         clean_graph()
         with db.atomic():
-            self._write_nodes()
-            self._write_edges()
+            self._write()
     
-    def _write_nodes(self) -> None:
-        for batch in chunked(self._get_node_data(), self.__NODES_PER_BATCH):
-            PlayNode.insert_many(batch).execute()
-
-    def _get_node_data(self) -> Iterable[Dict[str, Any]]:
-        interval_logger = _IntervalLogger(len(self._graph), "{} of {} nodes written")
-        for i, (play_id, data) in enumerate(self._graph.nodes(data=True)):
-            yield {
-                    "play_id": play_id,
-                    "outcome": data["outcome"]
-                }
-            interval_logger.log(i)
-
-    def _write_edges(self) -> None:
-        for batch in chunked(self._get_edge_data(), self.__EDGES_PER_BATCH):
-            PlayEdge.insert_many(batch).execute()
-
-    def _get_edge_data(self) -> Iterable[Dict[str, Any]]:
-        interval_logger = _IntervalLogger(self._graph.number_of_edges(), "{} of {} edges written")
-        for i, (from_id, to_id) in enumerate(self._graph.edges):
-            yield {
-                "from_id": from_id,
-                "to_id"  : to_id
-            }
-            interval_logger.log(i)
+    def _write(self) -> None:
+        for batch in chunked(PlayGraphDbIterator(), )
 
 class PlayGraphDbIterator:
     """Reads plays from the database and produces the corresponding node and
     edge data.
     """
-
-    def __init__(self):
-        self._graph: nx.DiGraph = None
 
     def __iter__(self):
         self._plays = self.__get_plays()
@@ -197,20 +168,12 @@ class _ChecksumGenerator:
         self._name = filename
         self._checksum = None
 
-    def get_checksum(self) -> Optional[str]:
-        if self._checksum is None:
-            try:
-                self.__init_checksum()
-            except FileNotFoundError:
-                return None
-        return self._checksum
-
-    def __init_checksum(self) -> None:
+    def get_checksum(self) -> str:
         md5sum = md5()
         with open(self._name, "rb") as fileobj:
             for chunk in iter(lambda: fileobj.read(self._BUFFER_SIZE), b""):
                 md5sum.update(chunk)
-        self._checksum = md5sum.hexdigest()
+        return md5sum.hexdigest()
 
 class _IntervalLogger:
 
