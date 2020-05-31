7ba63508c568756840aac7276775b2c1d27aa108
Ryan Longood <longoodr@gmail.com>
2020-05-31 16:04:23 -0400

    first draft of new rating system

--- deepfield/model/models.py
+++ deepfield/model/models.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from math import exp, log
 from typing import Dict, Iterable, List, Set, Tuple
 
 import numpy as np
@@ -78,87 +79,154 @@ class PlayerRatings:
 
     def __init__(self, num_stats: int):
         self._num_stats = num_stats
-        self._empty_rating = PlayerRating(num_stats)
         self.reset()
 
-    def get_pitcher_rating(self, pid: int) -> np.ndarray:
-        return self._get_player_rating(self._pratings, pid)
+    def get_batter(self, bid: int) -> PlayerRating:
+        if bid not in self._bratings:
+            self._bratings[bid] = PlayerRating(self._avg_batter)
+        return self._bratings[bid]
 
-    def get_batter_rating(self, bid: int) -> np.ndarray:
-        return self._get_player_rating(self._bratings, bid)
-
-    def _get_player_rating(self,
-                           ratings: Dict[int, "PlayerRating"],
-                           id_: int
-                           ) -> np.ndarray:
-        if id_ not in ratings:
-            return self._empty_rating.get_agg_rating()
-        return ratings[id_].get_agg_rating()
+    def get_pitcher(self, pid: int) -> PlayerRating:
+        if pid not in self._pratings:
+            self._pratings[pid] = PlayerRating(self._avg_pitcher)
+        return self._pratings[pid]
 
     def reset(self):
-        """Zeroes all player ratings."""
+        """Resets all player ratings."""
         self._bratings: Dict[int, "PlayerRating"] = {}
         self._pratings: Dict[int, "PlayerRating"] = {}
-        self._avg_batter = PlayerRating(self._num_stats)
-        self._avg_pitcher = PlayerRating(self._num_stats)
+        self._avg_batter = AvgPlayerRating()
+        self._avg_pitcher = AvgPlayerRating()
 
-    def update(self, delta: np.ndarray, bid: int, pid: int)\
+    def update(self, event: np.ndarray, bid: int, pid: int)\
             -> None:
         """Updates the ratings for the given players."""
-        self._update(delta, self._bratings, bid)
-        self._update(delta, self._pratings, pid)
-        self._avg_batter.update(delta)
-        self._avg_pitcher.update(delta)
+        self.get_batter(bid).update(event)
+        self.get_pitcher(pid).update(event)
+        self._avg_batter.update(event)
+        self._avg_pitcher.update(event)
 
-    def _update(self, delta, ratings: np.ndarray, id_: int) -> None:
-        if id_ not in ratings:
-            ratings[id_] = PlayerRating(self._num_stats)
-        ratings[id_].update(delta)
-
-    def get_node_pairwise_diffs(self, nodes: Iterable[Dict[str, int]]) -> np.ndarray:
-        """Returns the pairwise differences for the given nodes."""
-        diffs = [self.get_pairwise_diffs(n["batter_id"], n["pitcher_id"])
+    def get_matchup_ratings(self, nodes: Iterable[Dict[str, int]]) -> np.ndarray:
+        """Returns the matchup ratings for the given nodes."""
+        diffs = [self.get_matchup_rating(n["batter_id"], n["pitcher_id"])
                  for n in nodes]
         return np.stack(diffs)
 
-    def get_pairwise_diffs(self, bid: int, pid: int) -> np.ndarray:
-        """Returns the pairwise difference matrix for the given players."""
-        brating = self.get_batter_rating(bid)
-        prating = self.get_pitcher_rating(pid)
-        avg_pr = self._avg_pitcher.get_agg_rating()
-        avg_br = self._avg_batter.get_agg_rating()
-        return np.concatenate([brating - avg_br, prating - avg_pr], axis=None)
+    def get_matchup_rating(self, bid: int, pid: int) -> np.ndarray:
+        """Returns the matchup rating for the given players."""
+        brating = self.get_batter(bid)
+        prating = self.get_pitcher(pid)
+        b_vs_avg = brating.get_rating() - self._avg_batter.get_rating()
+        p_vs_avg = prating.get_rating() - self._avg_pitcher.get_rating()
+        return np.concatenate(
+                [
+                    b_vs_avg,
+                    p_vs_avg,
+                    brating.appearances,
+                    prating.appearances,
+                ],
+                axis=None
+            )
 
-class PlayerRating:
-    """A set of rating data for a given player."""
+class AbstractPlayerRating:
+    """Tracks rating data for matchups."""
 
-    SHORT_TERM_LENGTH = 100
-    LONG_TERM_LENGTH = 1000
-    CAREER_TERM_LENGTH = 10000
-
-    SHORT_TERM_WEIGHT = 1 / SHORT_TERM_LENGTH
-    LONG_TERM_WEIGHT = 1 / LONG_TERM_LENGTH
-    CAREER_TERM_WEIGHT = 1 / CAREER_TERM_LENGTH
-
-    def __init__(self, num_stats: int):
-        self._num_stats = num_stats
-        self.reset()
+    def __init__(self, subratings: Iterable["AbstractSubrating"]):
+        self._subratings = list(subratings)
 
     def reset(self) -> None:
-        """Resets this player's ratings and appearances to zero."""
-        self._short_term = np.zeros(self._num_stats)
-        self._long_term = np.zeros(self._num_stats)
-        self._career = np.zeros(self._num_stats)
+        """Resets the rating to its initial value."""
+        for subrating in self._subratings:
+            subrating.reset()
 
-    def update(self, delta: np.ndarray) -> None:
-        """Updates the player's ratings with the given delta."""
-        self._short_term = self.SHORT_TERM_WEIGHT * delta \
-                + (1 - self.SHORT_TERM_WEIGHT) * self._short_term
-        self._long_term = self.LONG_TERM_WEIGHT * delta \
-                + (1 - self.LONG_TERM_WEIGHT) * self._long_term
-        self._career = self.CAREER_TERM_WEIGHT * delta \
-                + (1 - self.CAREER_TERM_WEIGHT) * self._career
+    def update(self, event: np.ndarray) -> None:
+        """Updates the ratings with the given event. This should be a one-hot
+        vector.
+        """
+        for subrating in self._subratings:
+            subrating.update(event)
 
-    def get_agg_rating(self) -> np.ndarray:
-        ratings = [self._short_term, self._long_term, self._career]
-        return np.concatenate(ratings, axis=None)
+    def get_rating(self) -> np.ndarray:
+        """Returns the value of this rating."""
+        return np.concatenate(self._subratings, axis=None)
+
+class AvgPlayerRating(AbstractPlayerRating):
+    """Represents the rating of the average player at a given point in time."""
+
+    def __init__(self):
+        self.short_term = AvgPlayerSubrating(1000)
+        self.mid_term = AvgPlayerSubrating(10000)
+        self.long_term = AvgPlayerSubrating(100000)
+        subratings = [self.short_term, self.mid_term, self.long_term]
+        super().__init__(subratings)
+
+class PlayerRating(AbstractPlayerRating):
+    """A set of rating data for a given player."""
+
+    def __init__(self, avg_rating: AvgPlayerRating):
+        self.short_term = PlayerSubrating(100, avg_rating.short_term)
+        self.mid_term = PlayerSubrating(1000, avg_rating.mid_term)
+        self.long_term = PlayerSubrating(10000, avg_rating.long_term)
+        subratings = [self.short_term, self.mid_term, self.long_term]
+        super().__init__(subratings)
+        self.appearances = 0
+
+    def update(self, event: np.ndarray) -> None:
+        super().update(event)
+        self.appearances += 1
+
+class AbstractSubrating(ABC):
+    """A moving average of performance over a given number of plays."""
+
+    def __init__(self, plays_over: int):
+        self.rating = self._initial_value()
+        self._weight = 1 / plays_over
+
+    def update(self, event: np.ndarray) -> None:
+        update_funcs = [
+                self._pos_update if event[i] >= self.rating[i]
+                else self._neg_update
+                for i in range(event.size)
+            ]
+        self.rating = np.asarray([
+                update(x) for update, x in zip(update_funcs, self.rating)
+            ])
+
+    # see https://www.desmos.com/calculator/lffptb95tq for a visual explanation
+    # of these updates
+    def _pos_update(self, x):
+        return 1 - exp(log(1-x) - self._weight)
+
+    def _neg_update(self, x):
+        return exp(log(x) - self._weight)
+
+    def reset(self) -> None:
+        """Resets this rating to its initial value."""
+        self.rating = self._initial_value()
+
+    @abstractmethod
+    def _initial_value(self) -> np.ndarray:
+        """Returns the initial value of this rating."""
+        pass
+
+class AvgPlayerSubrating(AbstractSubrating):
+    """The average player's initial value is the percentage array for outcomes
+    over all plays in the database.
+    """
+
+    _init_val = Outcome.get_percentages()
+
+    def _initial_value(self) -> np.ndarray:
+        return self._init_val
+
+class PlayerSubrating(AbstractSubrating):
+    """A player's initial value is whatever the current average player rating
+    is.
+    """
+
+    def __init__(self, plays_over: int, avg_subrating: AvgPlayerSubrating):
+        self._avg = avg_subrating
+        super().__init__(plays_over)
+
+    def _initial_value(self):
+        return self._avg.rating
--- deepfield/model/trainer.py
+++ deepfield/model/trainer.py
@@ -40,22 +40,19 @@ while True:
         outcomes = np.asarray([n["outcome"] for n in level])
         one_hots = to_categorical(outcomes, num_classes=len(Outcome))
         y = Batcher.pad_batch(one_hots)
-        diffs = ratings.get_node_pairwise_diffs(level)
-        x = Batcher.pad_batch(diffs)
+        ratings = ratings.get_matchup_ratings(level)
+        x = Batcher.pad_batch(ratings)
         weights = Batcher.get_padded_weights(len(level))
         kl_divs = model.backprop(x, y, weights)
         tot_kl_div += np.sum(kl_divs)
         predictions = model.predict(x)[:len(level)]
         for i, (node, one_hot) in enumerate(zip(level, one_hots)):
             outcome = node["outcome"]
-            delta_scale = (one_hot - predictions[i])[outcome]
-            delta = delta_scale * one_hot
-            ratings.update(delta, node["batter_id"], node["pitcher_id"])
+            ratings.update(one_hot, node["batter_id"], node["pitcher_id"])
         num_seen += len(level)
         tot_seen += len(level)
         if num_seen >= reset_after:
             print(f"{tot_kl_div / num_seen:1.3f}, {num_seen:7d}, {passes:3d}, {tot_seen}")
             num_seen = 0
             tot_kl_div = 0
-    model.model.save("model")
     passes += 1
