ac22f867c30351dfe3da6dc2e2b302fe1a3e4c61
Ryan Longood <longoodr@gmail.com>
2020-05-31 17:30:50 -0400

    migrate rating classes

--- deepfield/playgraph/ratings.py
+++ deepfield/playgraph/ratings.py
@@ -1,76 +1,11 @@
 from abc import ABC, abstractmethod
 from math import exp, log
-from typing import Dict, Iterable, List, Set, Tuple
+from typing import Dict, Iterable
 
 import numpy as np
-from scipy.special import softmax
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Activation, Dense, Flatten, InputLayer
-from tensorflow.keras.losses import kullback_leibler_divergence as kl_div
-from tensorflow.keras.models import Model, Sequential, clone_model
-from tensorflow.keras.utils import to_categorical
 
 from deepfield.enums import Outcome
 
-
-class Batcher:
-    """Because of the variable-length levels inherent in the structure of
-    the play dependency graph, batch size is variable among training and
-    prediction calls to the model, which triggers retracing of the 
-    computation graph and slows down training. For this reason, batches
-    need to be made uniform size.
-    """
-
-    PAD_SIZE = 2 ** 6
-
-    @classmethod
-    def pad_batch(cls, batch: np.ndarray) -> np.ndarray:
-        """Rounds the given batch size up to the next power of 2 by padding the
-        empty batch samples with zeroes. Returns the padded batch.
-        """
-        unpadded_size: int = batch.shape[0]
-        if unpadded_size == cls.PAD_SIZE:
-            return batch
-        pad_length = cls.PAD_SIZE - unpadded_size
-        sample_shape = batch.shape[1:]
-        dup_samples = np.stack([np.zeros(sample_shape)for _ in range(pad_length)])
-        padded_batch = np.concatenate([batch, dup_samples])
-        return padded_batch
-
-    @classmethod
-    def get_padded_weights(cls, unpadded_size: int):
-        """Returns the sample weight array to ignore the samples when the given
-        batch is padded.
-        """
-        if unpadded_size == cls.PAD_SIZE:
-            return np.ones(cls.PAD_SIZE)
-        pad_length = cls.PAD_SIZE - unpadded_size
-        return cls._get_padded_weights(unpadded_size, pad_length)
-
-    @staticmethod
-    def _get_padded_weights(unpadded_size: int, pad_length: int) -> np.ndarray:
-        unpadded_weights = np.ones(unpadded_size)
-        weight_padding = np.zeros(pad_length)
-        return np.concatenate([unpadded_weights, weight_padding])
-
-class PredictionModel:
-    """A wrapper around a model which predicts outcome distributions from 
-    player stat pairwise differences.
-    """
-
-    def __init__(self, model: Model):
-        self.model = model
-
-    def backprop(self,
-                 pairwise_diffs: np.ndarray,
-                 outcomes: np.ndarray, 
-                 weights: np.ndarray
-            ) -> np.ndarray:
-        self.model.train_on_batch(pairwise_diffs, outcomes)
-        return K.eval(kl_div(K.variable(outcomes), self.model(pairwise_diffs)))
-
-    def predict(self, pairwise_diffs: np.ndarray) -> np.ndarray:
-        return K.eval(self.model(pairwise_diffs))
 
 class PlayerRatings:
     """A set of ratings for players that can be updated as plays are 
