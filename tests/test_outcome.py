8f5981a10149059c8d963eec3382b62642db447b
Ryan Longood <longoodr@gmail.com>
2020-05-31 14:44:23 -0400

    test percentage skeleton

--- tests/test_outcome.py
+++ tests/test_outcome.py
@@ -3,6 +3,9 @@ from typing import Tuple
 import pytest
 
 from deepfield.enums import Outcome
+from deepfield.playgraph.retrieval import PlayGraphPersistor
+
+from tests import utils
 
 STRIKEOUT = Outcome.STRIKEOUT
 LINEOUT = Outcome.LINEOUT
@@ -60,3 +63,13 @@ pairs = [
 def test_outcomes(pair: Tuple[str, Outcome]):
     desc, outcome = pair
     assert Outcome.from_desc(desc) == outcome
+
+def test_percentages():
+    utils.init_test_env()
+    with pytest.raises(RuntimeError):
+        Outcome.get_percentages()
+    utils.insert_cubs_game()
+    PlayGraphPersistor().ensure_consistency()
+    p = Outcome.get_percentages()
+    pass
+    
