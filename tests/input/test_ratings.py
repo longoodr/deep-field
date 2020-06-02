e8660c2e58f8468e0401965398a00aa22b8a05c4
Ryan Longood <longoodr@gmail.com>
2020-06-02 13:49:59 -0400

    add handedness tests

---
+++ tests/input/test_ratings.py
@@ -0,0 +1,53 @@
+import pytest
+
+from deepfield.dbmodels import Player
+from deepfield.enums import Handedness
+from deepfield.input.ratings import HandednessTracker
+from tests import utils
+
+def setup_module(_):
+    utils.init_test_env()
+
+def teardown_module(_):
+    utils.remove_files()
+
+L = Handedness.LEFT.value
+R = Handedness.RIGHT.value
+B = Handedness.BOTH.value
+hands = [
+        ((L, L), (L, L)),
+        ((L, R), (R, L)),
+        ((R, L), (L, R)),
+        ((R, R), (R, R)),
+        ((B, L), (L, R)),
+        ((B, R), (R, L)),
+        ((L, B), (L, L)),
+        ((R, B), (R, R)),
+        ((B, B), (L, R))
+    ]
+
+@pytest.mark.parametrize("pair", hands)
+def test_matchups(pair):
+    matchup, exp = pair
+    insert_batter(matchup[0])
+    insert_pitcher(matchup[1])
+    h = HandednessTracker()
+    act = h.get_bat_pit_against_handednesses(1, 2)
+    assert act == exp
+    utils.clean_db()
+
+def insert_batter(bats):
+    Player.create(**{
+            "name": "George Burdell",
+            "name_id": "burdege01",
+            "bats": bats,
+            "throws": Handedness.RIGHT.value,
+        })
+
+def insert_pitcher(throws):
+    Player.create(**{
+            "name": "George Burdell",
+            "name_id": "burdege02",
+            "bats": Handedness.RIGHT.value,
+            "throws": throws
+        })
